e4a2d8a649b5e567986168dfe8566f398f66eeaa
SparshChaurasia <sparshchaurasia0021@gmail.com>
2024-07-14 15:54:50 +0530

    added website opening features

--- code_functions.py
+++ code_functions.py
@@ -1,10 +1,12 @@
 import os
+import webbrowser
 from datetime import datetime
 import time
 import pyautogui
 
 DESKTOP_PATH = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
 
+# OS Functions
 def get_datetime(**args) -> str:
     """
     Gets current system date and time.
@@ -18,24 +20,21 @@ def get_datetime(**args) -> str:
     curr_datetime = datetime.now().strftime("%d %B, %Y %A, %I:%M %p")
     return curr_datetime
 
-def open_apps(app_name: str) -> None:
+
+def shutdown() -> None:
     """
-    Opens the given apps.
+    Shuts down the computer.
 
     Parameters:
-        string: app name
+        None
     Returns:
         None
     """
-    pyautogui.press("super")
-    time.sleep(0.1)
-    pyautogui.typewrite(app_name)
-    time.sleep(0.4)
-    pyautogui.press("enter")
+    os.system('shutdown -s')
 
 def screenshot(**args) -> None:
     """
-    Takes sreenshot of the current screen.
+    Takes sreenshot of the current screen and saves it to desktop.
 
     Parameters:
         None
@@ -48,5 +47,32 @@ def screenshot(**args) -> None:
             n += 1
     pyautogui.screenshot(f"{DESKTOP_PATH}\screenshot{n}.png")
 
-def music():
-    pass
+# Open Apps and Websites
+def open_apps(app_name: str) -> None:
+    """
+    Opens the given apps.
+
+    Parameters:
+        string: app_name
+    Returns:
+        None
+    """
+
+    print("app name:", app_name)
+    pyautogui.press("super")
+    time.sleep(0.1)
+    pyautogui.typewrite(app_name)
+    time.sleep(0.4)
+    pyautogui.press("enter")
+
+def open_websites(website_url: str) -> None:
+    """
+    Opens the given website.
+
+    Parameters:
+        string: website_url
+    Returns:
+        None
+    """
+    print("url:", website_url)
+    webbrowser.open(url=website_url, autoraise=True)
--- main.py
+++ main.py
@@ -2,22 +2,27 @@ import os
 
 import google.ai.generativelanguage as glm
 import google.generativeai as genai
+from google.generativeai.types.generation_types import StopCandidateException
 from dotenv import dotenv_values
 
-from code_functions import get_datetime, open_apps, screenshot
+from code_functions import get_datetime, open_apps, screenshot, open_websites, shutdown
 
 genai.configure(api_key=dotenv_values(".env")["API_KEY"])
-model = genai.GenerativeModel('gemini-1.5-flash-latest', tools=[get_datetime, open_apps, screenshot])
+model = genai.GenerativeModel('gemini-1.5-flash-latest', tools=[get_datetime, open_apps, screenshot, open_websites, shutdown])
 
 
 if __name__ == "__main__":
     chat = model.start_chat(enable_automatic_function_calling=True)
+    chat.send_message("You are an AI desktop assistant. Give informative and helpful responses in a few short and simple sentences without markdown.")
     while True:
-      try:
-        prompt = input("USER >> ")
-        print()
-        response = chat.send_message(prompt)
-        print("AI >> ", response.text)
-        print("----------------------------------------------------------------------------------------------")
-      except KeyboardInterrupt:
-        break
+        try:
+          prompt = input("USER >> ")
+          print()
+          response = chat.send_message(prompt)
+          print("AI >> ", response.text)
+          print("----------------------------------------------------------------------------------------------")
+        except StopCandidateException:
+            print("Inappropriate or harmful request.")
+        except KeyboardInterrupt:
+            break
+    
