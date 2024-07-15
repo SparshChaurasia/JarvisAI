155df97f0e95c3c9b72a8f43ec61ff20d0412c33
SparshChaurasia <sparshchaurasia0021@gmail.com>
2024-07-15 20:34:21 +0530

    created working sidebar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 
 
@@ -16,6 +16,7 @@ from code_functions import (add, get_datetime, multiply, open_apps,
 genai.configure(api_key=dotenv_values(".env")["API_KEY"])
 model = genai.GenerativeModel('gemini-1.5-flash-latest', tools=[get_datetime, open_apps, screenshot, open_websites, shutdown, add, multiply])
 
+# Text to Speech engine congfiguration
 engine = pyttsx3.init()
 
 voices = engine.getProperty('voices')
@@ -33,51 +34,67 @@ def voice_input() -> str:
         try:
             print("Recognizing...")
             query = r.recognize_google(audio, language="en-in")
-            print("USER >> ", query)
+            
             return query
         except Exception as e:
             return "Some error occurred during the input process ask the user to try again."
-
-def assistant_sidebar():
-    root = Tk()
     
-    w = 200 
-    h = 250
+
+def ai_response(prompt: str) -> None:  
+    try:
+        print("USER >> ", prompt)
+
+        response = chat.send_message(prompt)
+
+        print("AI >> ", response.text)
+
+        engine.say(response.text)
+        engine.runAndWait()
+
+        print("----------------------------------------------------------------------------------------------")
+    except StopCandidateException:
+        print("Inappropriate or harmful request.")
+
+        engine.say("Inappropriate or harmful request")
+        engine.runAndWait()
+
+if __name__ == "__main__":
+    chat = model.start_chat(enable_automatic_function_calling=True)
+    chat.send_message("You are an AI desktop assistant. Give informative and helpful responses in a few short and simple sentences. Do not include markdown or emojis in your response.")
+
+    root = tk.Tk()
+    
+    w = 250 
+    h = 300
     ws = root.winfo_screenwidth() 
     hs = root.winfo_screenheight() 
     x = ws - w
     y = hs - h
     root.geometry('%dx%d+%d+%d' % (w, h, x, y))
-
-    frm = ttk.Frame(root, padding=10)
-    frm.grid()
-    ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
     root.lift()
     root.call('wm', 'attributes', '.', '-topmost', True)
-    root.overrideredirect(True)
+    root.resizable(False, False)
+    # root.overrideredirect(True)
+    
+    frame = tk.Frame(root)
+    frame.pack()
+
+    inputtxt = tk.Text(
+                    frame,
+                    height=5,
+                    width=20, 
+                    font=("Source Code Pro", 15),
+                    bg="white"
+                ) 
+  
+    inputtxt.grid(row=0, column=0, columnspan=2) 
+    
+    def ai_response_wrapper():
+        ai_response(inputtxt.get(1.0, "end-1c") )
+        inputtxt.delete(1.0, tk.END)
+
+    tk.Button(frame, text="RUN", font=("Source Code Pro", 12, "bold"), command=ai_response_wrapper).grid(row=1, column=0, sticky="news")
+    tk.Button(frame, text="QUIT", font=("Source Code Pro", 12, "bold"), command=root.destroy).grid(row=1, column=1, sticky="news")
+
     root.mainloop()
-
-if __name__ == "__main__":
-    chat = model.start_chat(enable_automatic_function_calling=True)
-    chat.send_message("You are an AI desktop assistant. Give informative and helpful responses in a few short and simple sentences. Do not include markdown or emojis in your response.")
-    # assistant_sidebar()
-    while True:
-        try:
-            prompt = input("USER >> ")
-            print()
-            response = chat.send_message(prompt)
-            print("AI >> ", response.text)
-
-            engine.say(response.text)
-            engine.runAndWait()
-
-            print("----------------------------------------------------------------------------------------------")
-        except StopCandidateException:
-            print("Inappropriate or harmful request.")
-
-            engine.say("Inappropriate or harmful request")
-            engine.runAndWait()
-        except KeyboardInterrupt:
-            break
     
